a29b102a5acfe8baa59da0980c136d28172a8307
Jorge Luis Morales Reyes <jorge.morales@uam.edu.ni>
2026-05-16 09:19:13 -0600

    Controlador de productos + Interfaz Categoria

--- src/main/java/com/tienda_express/controller/ProductoController.java
+++ src/main/java/com/tienda_express/controller/ProductoController.java
@@ -1,18 +1,21 @@
 package com.tienda_express.controller;
 
 import com.tienda_express.modelo.Producto;
+import com.tienda_express.repository.CategoriaRepository;
 import com.tienda_express.service.ProductoService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/productos")
 @RequiredArgsConstructor
 public class ProductoController {
     private final ProductoService productoService;
+    private final CategoriaRepository categoriaRepository;
 
     @GetMapping
     public String listar(Model model){
@@ -23,6 +26,55 @@ public class ProductoController {
     @GetMapping("/nuevo")
     public String nuevo(Model model){
         model.addAttribute("producto", new Producto());
+        model.addAttribute("categorias", categoriaRepository.findAll());
         return "productos/formulario";
     }
+
+    @PostMapping("/guardar")
+    public String guardarProducto(@Valid @ModelAttribute Producto producto,
+                                  BindingResult result, Model model){
+        if(result.hasErrors()){
+            model.addAttribute("categorias", categoriaRepository.findAll());
+            return "productos/formulario";
+        }
+
+        productoService.guardar(producto);
+        return "redirect:/productos";
+    }
+
+    @GetMapping("/{id}")
+    public String verDetalle(@PathVariable Long id, Model model){
+        model.addAttribute("producto", productoService.buscarPorId(id));
+        model.addAttribute("categorias", categoriaRepository.findAll());
+
+        return "productos/formulario";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String mostrarFormularioEditar(@PathVariable Long id, Model model)
+    {
+        model.addAttribute("producto", productoService.buscarPorId(id));
+        model.addAttribute("categorias", categoriaRepository.findAll());
+        return "productos/formulario";
+    }
+
+    @PostMapping("/actualizar/{id}")
+    public String actualizarProducto(@PathVariable Long id, @Valid @ModelAttribute Producto producto,
+                                     BindingResult result, Model model){
+        if(result.hasErrors()){
+            model.addAttribute("categorias", categoriaRepository.findAll());
+            return "productos/formulario";
+        }
+
+        productoService.actualizar(id, producto);
+        return "redirect:/productos";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable Long id){
+        productoService.eliminar(id);
+        return "redirect:/productos";
+    }
+
+
 }
---
+++ src/main/java/com/tienda_express/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.tienda_express.repository;
+
+import com.tienda_express.modelo.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
